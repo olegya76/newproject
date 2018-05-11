4cab95e7eae0661cb61dbc91f73a484f200bb549
Oleg Yakuts <yakuts.oleg@gmail.com>
2018-05-11 17:40:31 +0300

    templates

--- testapp/views.py
+++ testapp/views.py
@@ -6,4 +6,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello World!")
+    my_dict={'insert_me':"Hello I am from testapp/index.html"}
+    return render(request,'testapp/index.html',context=my_dict)
