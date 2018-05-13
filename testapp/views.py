751ed1f3b90de27c24d1267471ce5d4cbbfc0b08
Oleg Yakuts <yakuts.oleg@gmail.com>
2018-05-13 21:03:07 +0300

    Example + static + bd

--- testapp/views.py
+++ testapp/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
-
-
+from django.http import HttpResponse
+from .models import *
 # Create your views here.
 
-from django.http import HttpResponse
+# Our original index view function
+# Corresponds to original_index.html (rename it to index.html to use it!)
+
+# def index(request):
+#     my_dict = {'insert_me':"Now I am coming from first_app/index.html!"}
+#     # Make sure this is pointing to the correct index
+#     return render(request,'first_app/index.html',context=my_dict)
+
 
 def index(request):
-    my_dict={'insert_me':"Hello I am from testapp/index.html"}
-    return render(request,'testapp/index.html',context=my_dict)
+    reklama_list = Reklama.objects.order_by('mail')
+    reklama_dict = {"reklama":reklama_list}
+    return render(request,'testapp/index.html',reklama_dict)
